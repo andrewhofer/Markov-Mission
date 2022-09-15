676a4bd51f743c8bc6b989308ce11bd413165d98
Drew Hofer <dhofer@bowdoin.edu>
2022-09-15 00:03:33 -0400

    OOP

--- Markov.py
+++ Markov.py
@@ -2,25 +2,40 @@ import numpy as np
 import random
 from PIL import Image
 
+class MarkovMalevich:
+
+    def __init__(self, pixels, width, height, mode):
+        self.pixels = pixels
+        self.output = pixels
+        self.width = width
+        self.height = height
+        self.mode = mode
+
+    def gen_new_pixels(self):
+        new_image = Image.new(self.mode, (self.width, self.height))
+        self.output = new_image.load()
+        return new_image
+
+    def gen_new_image(self):
+        # Print(pixels[0,0]) is the format for using pixel data
+        for x in range(self.width):
+            for y in range(self.height):
+                self.output[x, y] = self.pixels[x, y]
+
+
+
 def main():
     airplane_flying = Image.open("AirplaneFlying.jpg", 'r')
     width, height = airplane_flying.size
+
     airplane_flying_mode = airplane_flying.mode
-    
     pixels = airplane_flying.load()
-    #print(pixels[0,0]) is the format for using pixel data
 
-    new_image = Image.new(airplane_flying_mode, (width, height))
-    new_pixels = new_image.load()
+    markov_inspired_malevich = MarkovMalevich(pixels, width, height, airplane_flying_mode)
+    new_image = markov_inspired_malevich.gen_new_pixels()
+    markov_inspired_malevich.gen_new_image()
 
-    for x in range(width):
-        for y in range(height):
-            # Copy the original pixel to the new pixel map.
-            new_pixels[x, y] = pixels[x, y]
-            # new_pixels[x, y] = pixels[random.randint(0, width-1), random.randint(0, height-1)]
-            # random pixels
-
-    new_image.save('NewImage.png')
+    new_image.save('NewImage3.png')
 
 if __name__ == "__main__":
     main()
